745dc28d1461e1b7ce7ff66b8ea06261178e27c8
h679178 <679178@stud.hvl.no>
2024-03-14 11:31:16 +0100

    so og sei ferdig med oppgave 4

--- src/Oppgave_4/GenererHashKladd.java
+++ src/Oppgave_4/GenererHashKladd.java
@@ -1,25 +1,57 @@
 package Oppgave_4;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Random;
+
 public class GenererHashKladd {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 		// tabell med plass til 1_000_000 elementer av integers.
-		
-		int t1 = 376;
-		int t2 = 377;
-		int t3 = 999;
-		int t4 = 1;
-		
-		System.out.println(t1 + 500_001 % 1_000_000);
-		System.out.println(t2 + 500_001 % 1_000_000);
-		System.out.println(t3 + 500_001 % 1_000_000);
-		System.out.println(t4 + 500_001 % 1_000_000);
-		
+		ArrayList<Integer> intArr = new ArrayList<Integer>();
+		HashSet<Integer> intHash = new HashSet<Integer>();
+		int tall = 13;
+		for (int i = 0; i < 1_000_000; i++) {
+			intArr.add(tall);
+			intHash.add(tall);
+			tall = (tall + 45713) % 1_000_000;
+		}
+		long start = 0;
+		intArr.sort(null);
 
-		
-		
+		Random random = new Random(1);
+		int find = random.nextInt(100);
+		int iterations = 100_000;
+		int funnet = 0;
+		Object[] array = intArr.toArray();
+
+		int[] rand = new int[iterations];
+		for (int i = 0; i < iterations; i++) {
+			rand[i] = find;
+			find = random.nextInt(1, 1_000_000);
+		}
+
+		start = System.nanoTime();
+		for (int i = 0; i < iterations; i++) {
+			if (Arrays.binarySearch(array, rand[i]) >= 0) {
+				funnet++;
+			}
+		}
+		System.out.println("Binary Search brukte: " + (System.nanoTime() - start) / 1_000_000 + "ms \ntil og finne "
+				+ iterations + " elementer");
+		System.out.println("fant " + funnet + " elementer");
+		System.out.println();
+
+		funnet = 0;
+		start = System.nanoTime();
+		for (int i = 0; i < iterations; i++) {
+			if (intHash.contains(rand[i])) {
+				funnet++;
+			}
+		}
+		System.out.println("HashSet brukte: " + (System.nanoTime() - start) / 1_000_000 + "ms \ntil og finne "
+				+ iterations + " elementer");
+		System.out.println("fant " + funnet + " elementer");
 	}
-
 }
